cc35f43760ad22319c95aafe6e180317fac48aa9
FLI\nbaynham <nbaynham@flhi.com>
2019-10-29 15:42:08 -0400

    added RegEx challenges

--- src/test/java/io/nbaynham/hackerrank/TagContentExtractor.java
+++ src/test/java/io/nbaynham/hackerrank/TagContentExtractor.java
@@ -2,45 +2,31 @@ package io.nbaynham.hackerrank;
 
 import org.testng.annotations.Test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TagContentExtractor {
+
     private final String[] TEST_CASES = {
-        "<h1>Nayeem loves counseling</h1>",
         "<h1><h1>Sanjay has no watch</h1></h1><par>So wait for a while</par>",
+        "<h1>Nayeem loves counseling</h1>",
         "<Amee>safat codes like a ninja</amee>",
         "<SA premium>Imtiaz has a secret crush</SA premium>"
     };
 
     @Test
-    public void test() {
-        for (String item : TEST_CASES) {
-            System.out.println(getInnerText(item));  // getInnerText will handle everything recursively
+    public void testByRegex() {
+        String regex = "<(.+)>([^<]+)<\\/\\1>";
+        Pattern pattern = Pattern.compile(regex);
+        for (String testCase : TEST_CASES) {
+            Matcher matcher = pattern.matcher(testCase);
+            boolean found = false;
+            while(matcher.find()) {
+                System.out.println(matcher.group(2));
+                found = true;
+            };
+            if (!found) System.out.println("None");
         }
-    }
 
-    private String getInnerText(String text) {
-        // find the position of the text within the outer most tag
-        int leftPosition = findLeft(text);
-        int rightPosition = findRight(text);
-        if (leftPosition == -1 || rightPosition == -1) return text;     // no tags, so whatever text is available must be the answer
-        return getInnerText(text.substring(leftPosition, rightPosition));
-    }
-
-    private int findLeft(String text) {
-        for (int i = 0; i < text.length(); i++) {
-            if (text.substring(i, i+1).equals(">")) {
-                return i + 1;
-            }
-        }
-        return -1;
-    }
-
-    private int findRight(String text) {
-        for (int i = text.length() - 1; i > 1; i--) {
-            String nextString = text.substring(i - 1, i + 1);
-            if (nextString.equals("</")) {
-                return i - 1;
-            }
-        }
-        return -1;
     }
 }
